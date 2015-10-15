90f11db9180a5a077291ab00c1319899e61e196e
stunax <cmollgaard@gmail.com>
2015-10-15 18:52:52 +0200

    start

--- src/mr.erl
+++ src/mr.erl
@@ -12,7 +12,7 @@
 %% ---
 
 start() ->
-  ok.
+  gen_fsm:start(mr,arguments,).
 
 job(Pid, NumWork, MapFun, RedFun, Initial, Data) ->
   ok.
@@ -21,11 +21,43 @@ stop(Pid) ->
   gen_fsm:send_all_state_event(Pid, stop).
 
 advanced_job(Pid, NumWork, MapFun, RedFun, Initial, Data) ->
-  ok.
+  job(Pid, NumWork, MapFun, RedFun, Initial, Data).
 
 %% ---------
 %% Internals
 %% ---------
+
+%%Setup start
+init(_) ->
+  {ok,wait,[]}.
+
+
+terminate(A1,A2,A3) ->
+  ok.
+
+
+%%State function0
+stateName(A1,A2) ->
+  ok.
+
+%State function1
+stateName(A1,A2,A3) ->
+  ok.
+
+handle_event(Event,StateName,StateData) ->
+  ok.
+
+handle_sync_event(A1,A2,A3,A4) ->
+  ok.
+
+handle_info (A1,A2,A3) ->
+  ok.
+
+terminate(A1,A2,A3) ->
+  ok.
+
+code_change(A1,A2,A3,A4) ->
+  ok.
 
 %% --------------
 %% End of Module.
